bd40b5686d5eaf49698244730a4481d070ddebc6
hdainester <27729552+hdainester@users.noreply.github.com>
2019-03-12 12:51:09 +0100

    Monogame Xtended - initial commit

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Test {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using (var game = new TestGame())
+                game.Run();
+        }
+    }
+}
