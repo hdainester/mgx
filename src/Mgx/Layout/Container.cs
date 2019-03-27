07d959377b93ec1788b4ab2cef3429173355d9f9
hdainester <27729552+hdainester@users.noreply.github.com>
2019-03-27 13:14:43 +0100

    - new uml diagram: AlignChildren sequence

--- src/Mgx/Layout/Container.cs
+++ src/Mgx/Layout/Container.cs
@@ -59,31 +59,6 @@ namespace Mgx.Layout {
             }
         }
 
-        protected void _DefaultAlign() {
-            float w = 0, h = 0;
-
-            Children.ToList().ForEach(child => {
-                if(HGrow == 0 && child.HGrow == 0 && child.Width > w) w = child.Width;
-                if(VGrow == 0 && child.VGrow == 0 && child.Height > h) h = child.Height;
-            });
-            
-            if(HGrow == 0) Width = w;
-            if(VGrow == 0) Height = h;
-
-            Children.ToList().ForEach(child => {
-                if(child.HGrow > 0) _SetWidth(child, Math.Min(1, child.HGrow)*Width);
-                if(child.VGrow > 0) _SetHeight(child, Math.Min(1, child.VGrow)*Height);
-                if(child.HAlign == HAlignment.Left) _SetX(child, X);
-                if(child.HAlign == HAlignment.Center) _SetX(child, X + Width/2 - child.Width/2);
-                if(child.HAlign == HAlignment.Right) _SetX(child, X + Width - child.Width);
-                if(child.VAlign == VAlignment.Top) _SetY(child, Y);
-                if(child.VAlign == VAlignment.Center) _SetY(child, Y + Height/2 - child.Height/2);
-                if(child.VAlign == VAlignment.Bottom) _SetY(child, Y + Height - child.Height);
-            });
-        }
-
-        /// Default alignment for children is for each child
-        /// determined independent of the other children
         protected virtual void AlignChildren() {
             Containers.OrderBy(c => c.HGrow + c.VGrow)
                 .ToList().ForEach(c => {
@@ -125,6 +100,31 @@ namespace Mgx.Layout {
                 child.PropertyChanged -= ChildPropertyChangedHandler;
                 alignmentPending = true;
             }
+        }
+
+        /// Default alignment for children is for each child
+        /// determined independent of the other children
+        protected void _DefaultAlign() {
+            float w = 0, h = 0;
+
+            Children.ToList().ForEach(child => {
+                if(HGrow == 0 && child.HGrow == 0 && child.Width > w) w = child.Width;
+                if(VGrow == 0 && child.VGrow == 0 && child.Height > h) h = child.Height;
+            });
+            
+            if(HGrow == 0) Width = w;
+            if(VGrow == 0) Height = h;
+
+            Children.ToList().ForEach(child => {
+                if(child.HGrow > 0) _SetWidth(child, Math.Min(1, child.HGrow)*Width);
+                if(child.VGrow > 0) _SetHeight(child, Math.Min(1, child.VGrow)*Height);
+                if(child.HAlign == HAlignment.Left) _SetX(child, X);
+                if(child.HAlign == HAlignment.Center) _SetX(child, X + Width/2 - child.Width/2);
+                if(child.HAlign == HAlignment.Right) _SetX(child, X + Width - child.Width);
+                if(child.VAlign == VAlignment.Top) _SetY(child, Y);
+                if(child.VAlign == VAlignment.Center) _SetY(child, Y + Height/2 - child.Height/2);
+                if(child.VAlign == VAlignment.Bottom) _SetY(child, Y + Height - child.Height);
+            });
         }
     }
 }
